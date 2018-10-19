3b76b3c64bd86d2827c28d85262f616253d1add6
JROscar <a1783709064@163.com>
2018-10-19 15:20:28 +0800

    POJO 创建

---
+++ src/java/cn/oscar/po/TPerson.java
@@ -0,0 +1,57 @@
+package cn.oscar.po;
+
+
+public class TPerson {
+
+  private Integer id;
+  private String name;
+  private Integer age;
+  private String sex;
+  private Integer cardId;
+
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+
+  public Integer getAge() {
+    return age;
+  }
+
+  public void setAge(Integer age) {
+    this.age = age;
+  }
+
+
+  public String getSex() {
+    return sex;
+  }
+
+  public void setSex(String sex) {
+    this.sex = sex;
+  }
+
+
+  public Integer getCardId() {
+    return cardId;
+  }
+
+  public void setCardId(Integer cardId) {
+    this.cardId = cardId;
+  }
+
+}
